c9283018fdd93d4855dfd1fab756e6e0e5119ebe
Tobias Heinze <tobias.heinze99@gmail.com>
2018-08-09 23:53:31 +0200

    added gitignore

--- build/gen/build_info.c
+++ build/gen/build_info.c
@@ -1,4 +1,4 @@
 /* Auto-generated, do not edit. */
-const char *build_id = "20180809-214047/???";
-const char *build_timestamp = "2018-08-09T21:40:47Z";
+const char *build_id = "20180809-215116/???";
+const char *build_timestamp = "2018-08-09T21:51:16Z";
 const char *build_version = "1.6.3";
--- build/gen/mg_build_info.c
+++ build/gen/mg_build_info.c
@@ -1,4 +1,4 @@
 /* Auto-generated, do not edit. */
-const char *mg_build_id = "20180809-214047/???";
-const char *mg_build_timestamp = "2018-08-09T21:40:47Z";
+const char *mg_build_id = "20180809-215115/???";
+const char *mg_build_timestamp = "2018-08-09T21:51:15Z";
 const char *mg_build_version = "2018080921";
