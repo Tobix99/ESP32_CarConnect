45843ab23201501540ba1c63583ab3bbff91676b
Tobias Heinze <tobias.heinze99@gmail.com>
2018-08-09 22:58:36 +0200

    init push

---
+++ build/gen/build_info.c
@@ -0,0 +1,4 @@
+/* Auto-generated, do not edit. */
+const char *build_id = "20180809-003154/???";
+const char *build_timestamp = "2018-08-09T00:31:54Z";
+const char *build_version = "1.6.1";
---
+++ build/gen/mg_build_info.c
@@ -0,0 +1,4 @@
+/* Auto-generated, do not edit. */
+const char *mg_build_id = "20180809-003154/???";
+const char *mg_build_timestamp = "2018-08-09T00:31:54Z";
+const char *mg_build_version = "2018080900";
