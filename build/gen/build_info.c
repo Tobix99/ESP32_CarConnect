0a9212b0350413b60c482cd6f8fcbb1ff3330eb6
Tobias Heinze <tobias.heinze99@gmail.com>
2018-08-10 00:10:01 +0200

    added new bootstrap version and online functionality

--- build/gen/build_info.c
+++ build/gen/build_info.c
@@ -1,4 +1,4 @@
 /* Auto-generated, do not edit. */
-const char *build_id = "20180809-215116/???";
-const char *build_timestamp = "2018-08-09T21:51:16Z";
-const char *build_version = "1.6.3";
+const char *build_id = "20180809-220246/???";
+const char *build_timestamp = "2018-08-09T22:02:46Z";
+const char *build_version = "1.6.4";
